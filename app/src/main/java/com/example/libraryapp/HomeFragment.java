35481962f64784fc050a8893dfe2986bdb39c71f
Kevin Burke <22355634@studentmail.ul.ie>
2025-03-10 17:09:39 +0000

    removing onViewCreated

--- app/src/main/java/com/example/libraryapp/HomeFragment.java
+++ app/src/main/java/com/example/libraryapp/HomeFragment.java
@@ -10,14 +10,4 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         super(R.layout.fragment_home);
     }
-
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.bookSeat).setOnClickListener(v -> 
-            requireActivity().getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container_view, new BookingFragment())
-                .commit()
-        );
-    }
 }
