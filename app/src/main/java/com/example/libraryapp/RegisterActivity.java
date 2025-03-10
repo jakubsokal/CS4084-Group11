83cf4fae1c2e1116b6e279d47e0115ba1257b3a1
jasoncushen <22342516@studentmail.ul.ie>
2025-03-10 19:38:48 +0000

    updated register page

--- app/src/main/java/com/example/libraryapp/RegisterActivity.java
+++ app/src/main/java/com/example/libraryapp/RegisterActivity.java
@@ -1,5 +1,47 @@
 package com.example.libraryapp;
 
-public class RegisterActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText registerEmail, registerPassword, registerConfirmPassword;
+    private Button registerSubmit;
+    private TextView loginNow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        registerEmail = findViewById(R.id.register_email);
+        registerPassword = findViewById(R.id.register_password);
+        registerConfirmPassword = findViewById(R.id.register_confirm_password);
+        registerSubmit = findViewById(R.id.register_submit);
+        loginNow = findViewById(R.id.login_now_link);
+
+        registerSubmit.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            finish();
+        });
+
+        loginNow.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginPage.class));
+            finish();
+        });
+    }
 }
+
+
+
+
+
+
