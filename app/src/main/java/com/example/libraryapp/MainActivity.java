31bc36942fcd824709ce8a4949845b517f877043
jasoncushen <22342516@studentmail.ul.ie>
2025-04-25 02:22:13 +0100

    Changed layout and slight changes

--- app/src/main/java/com/example/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/libraryapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.libraryapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.libraryapp.db.booked.Booked;
 import com.example.libraryapp.db.users.Users;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Users s = new Users(this); //TESTING PURPOSE
+        //Users s = new Users(this); //TESTING PURPOSE
+        Booked booked = new Booked(this);
         Button login = findViewById(R.id.login);
         login.setOnClickListener(click -> {
             new Animation().animateButtonTint(click);
             Intent intent = new Intent(MainActivity.this, LoginPage.class);
             startActivity(intent);
+        });
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(click -> {
+            new Animation().animateButtonTint(click);
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "Register clicked", Toast.LENGTH_SHORT).show();
+
         });
     }
 }
